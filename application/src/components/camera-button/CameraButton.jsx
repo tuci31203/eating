9a6bb016f75d25349686face7fa75cf083a2ceae
tuci31203 <chienkt3112@gmail.com>
2024-11-18 19:59:57 +0700

    feat: refactor image upload logic and extract uploadImage function

--- application/src/components/camera-button/CameraButton.jsx
+++ application/src/components/camera-button/CameraButton.jsx
@@ -1,13 +1,11 @@
-import { StyleSheet, TouchableOpacity, View, Image, Alert } from 'react-native'
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native'
 import React, { useContext, useState } from 'react'
 import { ICONS } from '../../configs/constants/graphic'
-import { API_KEY, IMAGE_MODEL } from '@env';
 import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
 import { StateContext } from '../../context/StateContext';
+import { uploadImage } from '../../configs/networking/image-model/uploadImage';
 
 const CameraButton = () => {
-
     const [photo, setPhoto] = useState(null);
     const { ingredients, setIngredients } = useContext(StateContext)
 
@@ -20,55 +18,11 @@ const CameraButton = () => {
 
         if (!result.canceled) {
             setPhoto(result.assets[0]);
-            uploadImage(result.assets[0]);
+            const ingres = await uploadImage(result.assets[0]);
+            if (ingres && ingres.length > 0) setIngredients(ingres);
         }
     };
 
-    const uploadImage = async (image) => {
-        const formData = new FormData();
-        formData.append('model', IMAGE_MODEL);
-        formData.append('imgsz', '640');
-        formData.append('conf', '0.2');
-        formData.append('iou', '0.8');
-        formData.append('file', {
-            uri: image.uri,
-            type: 'image/jpeg',
-            name: 'photo.jpg',
-        });
-
-        try {
-            const response = await axios.post('https://predict.ultralytics.com', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'x-api-key': API_KEY,
-                },
-            });
-            const res = await response.data
-            if (res && res.error) {
-                console.log(res.error)
-                return
-            }
-            if (res && res["images"]) {
-                const imgs = res["images"][0]["results"]
-                console.log('======> IMAGES:\n', imgs)
-                const names = [...new Set(imgs.map(item => item.name))]
-                const ingres = names.map((item, index) => ({
-                    id: index + 1,
-                    name: item,
-                    amount: null,
-                    chosen: false,
-                    unit: 'g',
-                }))
-                console.log('======> NAMES:\n', names, '\n======> INGREDIENTS\n', ingres)
-                setIngredients(ingres);
-
-            }
-
-            console.log('Upload successful:');
-        } catch (error) {
-            console.error('Error uploading image:', error.response?.data || error.message);
-        }
-    }
 
     return (
         <View>
---
+++ application/src/configs/networking/image-model/uploadImage.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { API_KEY, IMAGE_MODEL } from '@env';
+import { capitalizeFirstLetter } from "../../../utils/capitalizeLetter";
+
+export const uploadImage = async (image) => {
+    const formData = new FormData();
+    formData.append('model', IMAGE_MODEL);
+    formData.append('imgsz', '640');
+    formData.append('conf', '0.2');
+    formData.append('iou', '0.8');
+    formData.append('file', {
+        uri: image.uri,
+        type: 'image/jpeg',
+        name: 'photo.jpg',
+    });
+
+    try {
+        const response = await axios.post('https://predict.ultralytics.com', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'x-api-key': API_KEY,
+            },
+        });
+        const res = await response.data
+        if (res && res.error) {
+            console.log(res.error)
+            return
+        }
+        if (res && res["images"]) {
+            const imgs = res["images"][0]["results"]
+            // console.log('======> IMAGES:\n', imgs)
+            const names = [...new Set(imgs.map(item => capitalizeFirstLetter(item.name)))]
+            const ingres = names.map((item, index) => ({
+                id: index + 1,
+                name: item,
+                amount: null,
+                chosen: false,
+                unit: 'g',
+            }))
+            console.log('======> NAMES:\n', names)
+            return ingres
+        }
+        return
+        console.log('Upload successful:');
+    } catch (error) {
+        console.error('Error uploading image:', error.response?.data || error.message);
+        return
+    }
+}
