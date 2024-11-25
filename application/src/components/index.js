8bc278986b5ff016279a51df2e2548ae9a29489e
tuci31203 <chienkt3112@gmail.com>
2024-11-25 12:09:59 +0700

    feat: add index file to export common components for easier imports

---
+++ application/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as GreetText } from './GreetText'
+export { default as Button } from './Button'
+export { default as ScreenView } from './ScreenView'
+export { default as Xmark } from './navigation/Xmark'
